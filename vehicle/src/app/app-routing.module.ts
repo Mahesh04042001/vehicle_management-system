1c2142c78dae471669d9e5bfef60c2dbb9eb9eeb
Maheshwaran S <mahe2998@global.chainsys.com>
2022-05-10 18:17:56 +0530

    project

--- vehicle/src/app/app-routing.module.ts
+++ vehicle/src/app/app-routing.module.ts
@@ -4,23 +4,17 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HomemenulayoutComponent } from './homemenulayout/homemenulayout.component';
 import { LogInComponent } from './log-in/log-in.component';
-
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { AdminpageComponent } from './adminpage/adminpage.component';
 const routes: Routes = [
-  // {
-  //   path:'',component:HomemenulayoutComponent
-  // },
-  {
-    path:'home',component:HomeComponent
-  },
-  {
-    path:'app',component:AppComponent
-  },
-  {
-    path:'login',component:LogInComponent
-  },
-  {
-    path:'login/app',component:AppComponent
-  }
+  {path:'home',component:HomeComponent},
+  {path:'contactus',component:ContactUsComponent},
+  {path:'aboutus',component:AboutUsComponent},
+  {path:'app',component:AppComponent},
+  {path:'login',component:LogInComponent},
+  {path:'login/app',component:AppComponent},
+  {path:'adminpage',component:AdminpageComponent}
 ];
 
 @NgModule({
